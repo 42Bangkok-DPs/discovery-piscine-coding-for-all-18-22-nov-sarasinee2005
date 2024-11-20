29f145666496d1dee077d1159b023940faf5b53d
sarasinee2005 <my.sarasinee@gmail.com>
2024-11-20 17:48:05 +0000

    main

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,8 +1,11 @@
-number = float(input("Enter a number: "))
-
 if number > 0:
-    print("This number is positive.")
-elif number < 0:
-    print("This number is negative.")
-else:
-    print("This number is both positive and negative.")
+            print("This number is positive.")
+        elif number < 0:
+            print("This number is negative.")
+        else:
+            print("This number is both positive and negative.")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+if name == "main":
+    main()
