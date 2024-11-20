f0c6dd6c5944b55f9b4b995ddfc0e7a3db75cb4d
sarasinee2005 <my.sarasinee@gmail.com>
2024-11-20 15:07:45 +0000

    main

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,12 +1,6 @@
-# Script: iszero.py
-
-# Prompt the user for a number
 number = input("Enter a number: ")
-
-# Try to convert the input to an integer
 try:
     number = int(number)
-    # Check if the number is equal to zero
     if number == 0:
         print("This number is equal to zero.")
     else:
