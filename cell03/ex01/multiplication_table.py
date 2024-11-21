0f5d53fbd55f31feadd0640974fd5b0fc87d5c0b
sarasinee2005 <my.sarasinee@gmail.com>
2024-11-21 06:08:16 +0000

    main

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,4 +1,9 @@
-number = int(input("Enter a number\n"))
+def multiplication_table():
+    number = int(input("Enter a number"))
 
-for i in range(10):
-    print(f"{i} x {number} = {i * number}")
+    for i in range(10):
+        result = number * i
+        print(f"{i} x {number} = {result}")
+
+if __name__ == "_main_":
+    multiplication_table()
