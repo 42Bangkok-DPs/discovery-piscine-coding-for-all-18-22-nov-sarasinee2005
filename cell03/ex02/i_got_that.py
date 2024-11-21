3a98688f620f3f5ce77a845c0c4a19dc3f407bb8
sarasinee2005 <my.sarasinee@gmail.com>
2024-11-21 06:47:35 +0000

    main

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,9 +1,9 @@
 def main():
-    while True :
-        user_input = input("What you gotta say?")
-        if user_input.lower() == "stop" :
+    while True:
+        user_input = input("What you gotta say? ")
+        if user_input.lower() == "stop":
             break
         print("I got that! Anything else?")
 
-if __name__ == "_main_" :
+if __name__ == "__main__":
     main()
