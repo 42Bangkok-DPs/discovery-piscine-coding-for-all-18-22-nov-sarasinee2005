744f2fa727294161a3dd9ea138fe24b2212f8eb3
sarasinee2005 <my.sarasinee@gmail.com>
2024-11-21 05:18:16 +0000

    main

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,11 +1,16 @@
-num1 = int(input())
-num2 = int(input())
-result = num1 + num2
+num1 = int(input("Enter the first number:\n"))
+num2 = int(input("Enter the second number:\n"))
+
+# Calculate the multiplication result
+result = num1 * num2
+
+# Display the multiplication result
 print(f"{num1} x {num2} = {result}")
 
-if result > 0 :
+# Determine if the result is positive, negative, or zero
+if result > 0:
     print("The result is positive.")
-elif result < 0 :
+elif result < 0:
     print("The result is negative.")
-else :
+else:
     print("The result is positive and negative.")
